3a83cf2821da319a19af085249655f06e6074b99
SivaguruAdaikkalam <sivaguruadaikkalam2511@gmail.com>
2026-03-16 22:26:46 +0530

    UC1: Username availability checker using HashMap

--- src/HashTable.java/HashTable.java
+++ src/HashTable.java/HashTable.java
@@ -1,7 +1,38 @@
+import java.util.*;
+
 public class HashTable {
 
-    public static void main(String[] args) {
-        System.out.println("Base Application Setup");
+    // UC1: Username Availability Checker
+    static HashMap<String,Integer> users = new HashMap<>();
+    static HashMap<String,Integer> attempts = new HashMap<>();
+
+    public static void usernameAvailabilityChecker() {
+
+        users.put("john_doe",1);
+        users.put("admin",2);
+
+        String username="john_doe";
+
+        attempts.put(username, attempts.getOrDefault(username,0)+1);
+
+        if(users.containsKey(username)){
+            System.out.println("Username already taken");
+
+            System.out.println("Suggestions:");
+            System.out.println(username+"1");
+            System.out.println(username+"2");
+            System.out.println(username.replace("_","."));
+        }
+        else{
+            System.out.println("Username available");
+        }
+
+        System.out.println("Attempt count: "+attempts.get(username));
     }
 
+    public static void main(String[] args) {
+
+        usernameAvailabilityChecker();
+
+    }
 }
