5090061869fb64c8dbcc1bed23a082326a8a355b
SivaguruAdaikkalam <sivaguruadaikkalam2511@gmail.com>
2026-03-16 22:38:43 +0530

    UC6: Distributed rate limiter using HashMap

--- src/HashTable.java/HashTable.java
+++ src/HashTable.java/HashTable.java
@@ -2,33 +2,27 @@ import java.util.*;
 
 public class PalindromeCheckerApp {
 
-    // UC5: Real-Time Analytics Dashboard
-    public static void realTimeAnalyticsDashboard(){
+    // UC6: Distributed Rate Limiter
+    public static void rateLimiter(){
 
-        HashMap<String,Integer> pageViews = new HashMap<>();
-        HashMap<String,Set<String>> uniqueVisitors = new HashMap<>();
-        HashMap<String,Integer> trafficSources = new HashMap<>();
+        HashMap<String,Integer> requestCount = new HashMap<>();
 
-        String page = "/article/breaking-news";
-        String user = "user_123";
-        String source = "google";
+        String clientId = "abc123";
 
-        pageViews.put(page, pageViews.getOrDefault(page,0)+1);
+        requestCount.put(clientId, requestCount.getOrDefault(clientId,0)+1);
 
-        uniqueVisitors.putIfAbsent(page,new HashSet<>());
-        uniqueVisitors.get(page).add(user);
-
-        trafficSources.put(source, trafficSources.getOrDefault(source,0)+1);
-
-        System.out.println("Page Views: " + pageViews);
-        System.out.println("Unique Visitors: " + uniqueVisitors.get(page).size());
-        System.out.println("Traffic Sources: " + trafficSources);
+        if(requestCount.get(clientId) <= 5){
+            System.out.println("Request allowed. Remaining requests: " + (5 - requestCount.get(clientId)));
+        }
+        else{
+            System.out.println("Rate limit exceeded for client: " + clientId);
+        }
     }
 
     public static void main(String[] args){
 
-        System.out.println("UC5");
-        realTimeAnalyticsDashboard();
+        System.out.println("UC6");
+        rateLimiter();
 
     }
 }
